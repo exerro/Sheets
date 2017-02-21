ec4d945e9a0519d385bb7bf667b31232564cee68
Benedict Allen <benedict.allen2514@gmail.com>
2017-02-21 20:51:37 +0000

    Add name and main options to `sbs init`

--- bin/cmd/init.lua
+++ bin/cmd/init.lua
@@ -6,10 +6,12 @@ sbs init
  - Initialises a new project
 
 Usage
- > init <project name> [--open] [--force] [--version <version>] [--silent]
- --> '--open' to open the project after creation
- --> '--force' to overwrite an existing project
+ > init <project name> [-option]
  --> '--author' <author> to define project author
+ --> '--name' <name> to give a custom project name
+ --> '--open' to open the project after creation
+ --> '--main' to add `main` file after creation
+ --> '--force' to overwrite an existing project
  --> '--version' to specify version, defaults to 'stable'
  --> '--silent' to hide terminal output
  > init help|-h|--help
@@ -27,9 +29,11 @@ if h == "-h" or h == "help" or h == "--help" then
 	return print( HELP )
 end
 
-parser:set_param_count( 1, 1 )
-parser:add_section( "open" ):set_param_count( 0, 0, "open" )
+parser:set_param_count( 0, 1 )
 parser:add_section( "author" ):set_param_count( 0, 1, "author" )
+parser:add_section( "name" ):set_param_count( 0, 1, "name" )
+parser:add_section( "open" ):set_param_count( 0, 0, "open" )
+parser:add_section( "main" ):set_param_count( 0, 0, "main" )
 parser:add_section( "force" ):set_param_count( 0, 0, "force" )
 parser:add_section( "version" ):set_param_count( 0, 1, "version" )
 parser:add_section( "silent" ):set_param_count( 0, 0, "silent" )
@@ -46,9 +50,9 @@ parser:set_param_validator( function( v )
 end, "version" )
 
 local parameters = parser:parse( ... )
-local name_raw = parameters[1]:gsub( "%.", "/" )
-local name = name_raw:gsub ".+/", ""
-local path = shell.resolve( name_raw )
+local name_raw = shell.resolve( parameters[1] or "" )
+local name = parameters.name or name_raw:gsub( ".+/", "" )
+local path = name_raw
 local ver = parameters.version or version( "resolve", "stable", "--silent" )
 
 if fs.exists( path .. "/.project_conf.txt" ) then
@@ -59,14 +63,14 @@ if fs.exists( path .. "/.project_conf.txt" ) then
 		if parameters.silent then
 			return false
 		else
-			return error( "Directory '" .. name .. "' already exists, use --force to overwrite", 0 )
+			return error( "Directory '" .. path .. "' already exists, use --force to overwrite", 0 )
 		end
 	end
 end
 
 if not parameters.silent then
 	print( "Initialising " .. name .. " @ " .. path )
-	print( "Using Sheets " .. ver )
+	print( "Using sheets " .. ver )
 end
 
 if not version( "exists", ver, "--silent" ) then
@@ -82,7 +86,7 @@ conf:write( "name", name )
 conf:write( "author", parameters.author or "anonymous" )
 conf:write( "version", "v0.0.1" )
 conf:write( "sheets_version", ver )
-conf:write( "files", {} )
+conf:write( "files", { parameters.main and "main" or nil } )
 conf:write( "flags.SHEETS_CORE_ELEMENTS", true )
 conf:write( "flags.SHEETS_THREADING", true )
 conf:write( "flags.SHEETS_LOWRES", true )
@@ -90,5 +94,5 @@ conf:write( "flags.SHEETS_SML", false )
 conf:close()
 
 if parameters.open then
-	open( parameters[1] )
+	open( "/" .. path )
 end
